0ea015d6cccc95b71b512a1a493025aab45e4bc4
Marco <marco@theanarchistlibrary.org>
2015-04-09 09:27:58 +0200

    Added helper

---
+++ s/ss/create-slides.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+muse-compile.pl --ttdir yapc-templates --tex slides.muse
+
+if [ ! -f slides.tex ]; then
+    echo "No slides found!"
+    exit 2
+fi
+
+# hacky
+sed -i 's/\\section{/\\end{frame}\n\n\\begin{frame}{/' slides.tex
+
+xelatex slides.tex
+xelatex slides.tex
+xelatex slides.tex
+
