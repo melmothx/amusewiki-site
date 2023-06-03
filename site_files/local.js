165a483cf326e5d205eb8cee42a787b68f8cb3c6
Marco Pessotto <marco@pessotto.hr>
2023-06-03 11:13:12 +0200

    Version 2.574

--- site_files/local.js
+++ site_files/local.js
@@ -1,2 +1,10 @@
 /* Empty by default, for local changes */
-
+$(document).ready(function() {
+    var current = window.location.pathname;
+    $('.amusewiki-site-nav-right').children().each(function() {
+        var el = $(this);
+        if (el.attr('href') === current) {
+            el.addClass('active');
+        }
+    });
+});
